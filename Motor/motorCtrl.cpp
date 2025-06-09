b64cf1227e0386cbc2d8a2f1dfcdb9057ef8c1e6
L KO <koksi9972@naver.com>
2025-06-10 02:08:35 +0900

    curve 함수 제외하고 모두 구현 완료. 제 1안 : r_pwm과 l_pwm 을 직접 제어하여 커브 제 2안 : 평균 pwm값과 각도값을 넣어 curve함수 내부에서 r_pwm, l_pwm을 계산하도록 하여 커브
    제 2안이 훨씬 간결하기에 2안으로 고민중임.. 공식과 원리 생각해보아야함

--- Motor/motorCtrl.cpp
+++ Motor/motorCtrl.cpp
@@ -1,8 +1,4 @@
-#include <wiringPi.h>
-#include <softPwm.h>
-#include <iostream>
-#include <motorCtrl.h>
-
+#include "motorCtrl.h"
 #define RPWM 1
 #define LPWM 23
 #define LR_EN 4
@@ -12,28 +8,39 @@
 
 #define maxPulse 255
 
+//@brief pwm값 유효성 확인
+#define validate_pwm(pwm)                         \
+    if (!pwm_isvalid(pwm)) {                            \
+        std::cout << "pwm value is invaild\n";       \
+        return;                                                      \
+    }
 // R, L MOtor 하나의 클래스에 넣어서 조져야함
 // R, L Motor in one class
 
-void Motor::motor_setup()
-{
-    wiringPiSetup();
-    pinMode(RPWM, OUTPUT);
-    pinMode(LPWM, OUTPUT);
-    pinMode(LL_EN, OUTPUT);
-    pinMode(LR_EN, OUTPUT);
-    pinMode(RR_EN, OUTPUT);
-    pinMode(RL_EN, OUTPUT);
 
-    digitalWrite(RR_EN, HIGH);
-    digitalWrite(RL_EN, LOW);
-    digitalWrite(LL_EN,LOW);
-    digitalWrite(LR_EN,HIGH);
+// private
+#pragma region Private functions
+void Motor::motor_setup(int r_pwmPin, int l_pwmPin, int rr_enPin, int rl_enPin, int lr_enPin, int ll_enPin) {
+    wiringPiSetup();
+    pinMode(r_pwmPin, OUTPUT);
+    pinMode(l_pwmPin, OUTPUT);
+    pinMode(rr_enPin, OUTPUT);
+    pinMode(rl_enPin, OUTPUT);
+    pinMode(lr_enPin, OUTPUT);
+    pinMode(ll_enPin, OUTPUT);
+
+    lmotor_direction_front();
+    rmotor_direction_front();
 
     //R_Motor, L_Motor 0으로 초기화, 최대 펄스 255
     //R_Motor, L_Motor initialize 0, max purse 255
-    softPwmCreate(RPWM, 0, maxPulse);
-    softPwmCreate(LPWM, 0, maxPulse);
+    softPwmCreate(r_pwmPin, 0, maxPulse);
+    softPwmCreate(l_pwmPin, 0, maxPulse);
+}
+
+void Motor::motor_setup()
+{
+    motor_setup(RPWM, LPWM, RR_EN, RL_EN, LR_EN, LL_EN);
 }
 
 bool Motor::pwm_isvalid(int pwm)
@@ -45,34 +52,119 @@ bool Motor::pwm_isvalid(int pwm)
     }
 }
 
-Motor::Motor()
+void Motor::lmotor_direction_front(bool front=true)
 {
+    if (front == true) {
+        digitalWrite(LR_ENPin, HIGH);
+        digitalWrite(LL_ENPin, LOW);
+    }
+    else {
+        digitalWrite(LR_ENPin, HIGH);
+        digitalWrite(LL_ENPin, LOW);
+    }
 }
 
-Motor::Motor(int pwmPin, int R_EN, int L_EN){
+void Motor::rmotor_direction_front(bool front=true)
+{
+    if (front == true) {
+        digitalWrite(RR_ENPin, HIGH);
+        digitalWrite(RL_ENPin, LOW);
+    }
+    else {
+        digitalWrite(RR_ENPin, HIGH);
+        digitalWrite(RL_ENPin, LOW);
+    }
+}
+#pragma endregion
 
+// public
+#pragma region Constructor functions
+Motor::Motor()
+{
+    motor_setup();
+    std::cout << "motor set up : default\n";
+}
+
+Motor::Motor(int r_pwmPin, int l_pwmPin, int rr_enPin, int rl_enPin, int lr_enPin, int ll_enPin){
+    
+    motor_setup(int r_pwmPin, int l_pwmPin, int rr_enPin, int rl_enPin, int lr_enPin, int ll_enPin);
+    std::cout << "motor set up : user setting\n";
 }
 
 Motor::~Motor(){
+    std::cout << "motor instance deleted\n";
+}
+#pragma endregion
 
+#pragma region Move functions
+void Motor::straight(int pwm)
+{
+    validate_pwm(pwm);
+    softPwmWrite(R_pwmPin, pwm);
+    softPwmWrite(L_pwmPin, pwm);
 }
 
-void Motor::straight(int pwm, int time)
+void Motor::backoff(int pwm)
 {
-    if(pwm_isvalid(pwm)==false){
-        std::cout<<"pwm value is invaild\n";
-        return;
+    validate_pwm(pwm);
+    rmotor_direction_front(false);
+    lmotor_direction_front(false);
+    softPwmWrite(R_pwmPin, pwm);
+    softPwmWrite(L_pwmPin, pwm);
+}
+
+void Motor::rotate(int pwm, float degree)
+{
+    validate_pwm(pwm);
+    if (degree > 0) 
+    {
+        rmotor_direction_front(false);
+        lmotor_direction_front();
     }
-    else{
-        softPwmWrite(RPWM, pwm);
-        delay(time);
+    else {
+        rmotor_direction_front();
+        lmotor_direction_front(false);
     }
+    // 모터 최대 150rpm, 설정 최대 pwm 255, 6.0f는 초당 각속도 계산을 위한 상수
+    float dgrspeed = 150.0f / 255.0f * pwm * 6.0f;
+    float delaytime = degree / dgrspeed;
+
+    softPwmWrite(R_pwmPin, pwm);
+    softPwmWrite(L_pwmPin, pwm);
+
+    delay(delaytime);
+
 }
 
-void Motor::straight(int pwm, bool func)
+// 커브 함수 동작 지침
+// 1. degree값만큼 회전
+// 2. 각 바퀴가 pwm값만큼 회전 (딜레이 시간 계산 필요)
+// 3. -degree값만큼 회전
+
+// l_pwm, r_pwm 말고, 원하는 pwm값 (오,왼 평균pwm), 각도를 설정해서 커브를 구현하는건 어떨까?
+
+
+void Motor::left_curve(int pwm)
+{
+
+}
+
+void Motor::right_curve(int pwm)
 {
 }
 
-void Motor::rotate(int pwm, int time)
+void Motor::left_curve(int l_pwm, int r_pwm)
 {
 }
+
+void Motor::right_curve(int l_pwm, int r_pwm)
+{
+}
+
+void Motor::curve(bool recover=false, int pwm, float degree)
+{
+    
+}
+
+#pragma endregion
+
--- Motor/motorCtrl.h
+++ Motor/motorCtrl.h
@@ -1,20 +1,60 @@
 #pragma once
 
+#include <wiringPi.h>
+#include <softPwm.h>
+#include <iostream>
+#include <motorCtrl.h>
+#include <chrono>
+#include <functional>
+
 class Motor{
     private:
-        int pwmPin;
-        int R_EN;
-        int L_EN;
+        int R_pwmPin;
+        int L_pwmPin;
+        int RR_ENPin;
+        int RL_ENPin;
+        int LR_ENPin;
+        int LL_ENPin;
 
         void motor_setup();
+        void motor_setup(int r_pwmPin, int l_pwmPin, int rr_enPin, int rl_enPin, int lr_enPin, int ll_enPin)
         bool pwm_isvalid(int pwm);
+
+        //@brief tip : if want to direction backward, input false
+        void lmotor_direction_front(bool front);
+        //@brief tip : if want to direction backward, input false
+        void rmotor_direction_front(bool front);
+
     public:
         Motor();
-        Motor(int pwmPin, int R_EN, int L_EN);
+        Motor(int r_pwmPin, int l_pwmPin, int rr_enPin, int rl_enPin, int lr_enPin, int ll_enPin);
         ~Motor();
 
-        void straight(int pwm, int time);
-        void straight(int pwm, bool func);
-        void rotate(int pwm, int time);
+        void straight(int pwm);
+
+       // @brief 후진
+       // @brief back off
+        void backoff(int pwm);
+
+        // 제자리 회전 : 오른쪽 + 왼쪽 -
+        // @brief rotate by degree : R positive, L negative
+        void rotate(int pwm, float degree);
+        
+        // @brief 기본 커브 default curve (30 degree)
+        void left_curve(bool recover, int pwm);
+        // @brief 기본 커브 default curve (30 degree)
+        void right_curve(bool recover, int pwm);
+
+        // @brief 사용자 지정커브 ( 사용자가 세팅한 pwm 값의 차이는 곡률에 영향을 줌 )
+        // @brief user setting curve ( user setting pwm diffrence provokes curvature )
+        void left_curve(bool recover,  int l_pwm, int r_pwm);
+        // @brief 사용자 지정커브 ( 사용자가 세팅한 pwm 값의 차이는 곡률에 영향을 줌 )
+        // @brief user setting curve ( user setting pwm diffrence provokes curvature )
+        void right_curve(bool recover, int l_pwm, int r_pwm);
+
+        // 커브의 새로운 함수.. 원하는 pwm(오,왼 평균), degree 값으로 
+        // 함수 내에서 자동으로 l, r pwm을 설정해주고 이동을 수행해줄 수 있도록..!
+        //@brief recover를 true로 지정해야 본래 각도로 되돌아옴
+        void curve(bool recover, int pwm, float degree);
         
 };
